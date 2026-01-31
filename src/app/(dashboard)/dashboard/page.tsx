00602791f98084bc417d4e566430f3d4700a9c39
Pongo <echotech8310@gmail.com>
2026-01-31 14:10:26 +0700

    refactor: remove user decoding logic and streamline token validation in Dashboard component

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,13 +1,18 @@
 "use server";
 import { cookies } from "next/headers";
 import DashboardClientPage from "./pageClient";
-import { decodeToken } from "@/app/_actions/authActions";
+import { permanentRedirect } from "next/navigation";
+import { authUseCase } from "@/di/modules";
 
 export default async function Dashboard() {
     const storeToken = (await cookies());
     const token = storeToken.get("next-auth.session-token")?.value;
-    const user = await decodeToken(token as string);
+
+    if (!token) {
+        permanentRedirect("/login");
+    }
+
     return (
-        <DashboardClientPage user={user} />
+        <DashboardClientPage />
     )
 }
