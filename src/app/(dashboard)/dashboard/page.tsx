eefd6d135df175c7fd89b07f9f10711231b3e266
Pongo <echotech8310@gmail.com>
2026-01-31 16:39:43 +0700

    refactor: remove unnecessary console log in Dashboard and add session log in DashboardClientPage

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -2,12 +2,11 @@
 import { cookies } from "next/headers";
 import DashboardClientPage from "./pageClient";
 import { permanentRedirect } from "next/navigation";
-import { authUseCase } from "@/di/modules";
 
 export default async function Dashboard() {
     const storeToken = (await cookies());
     const token = storeToken.get("next-auth.session-token")?.value;
-
+    console.log(token);
     if (!token) {
         permanentRedirect("/login");
     }
--- src/app/(dashboard)/dashboard/pageClient.tsx
+++ src/app/(dashboard)/dashboard/pageClient.tsx
@@ -12,6 +12,7 @@ import { useSession } from "next-auth/react";
 
 export default function DashboardClientPage() {
     const { data: session, status } = useSession();
+    console.log(session);
     if (status === "loading") return <div>Loading...</div>;
     const users = session?.user;
 
