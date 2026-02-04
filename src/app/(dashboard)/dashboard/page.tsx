0899f6489feb1edaa60fd8ffa7ba7c7fa2ca7d14
Pongo <echotech8310@gmail.com>
2026-02-04 16:23:06 +0700

    update dashboard

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,15 +1,7 @@
 "use server";
-import { cookies } from "next/headers";
 import DashboardClientPage from "./pageClient";
-import { permanentRedirect } from "next/navigation";
 
 export default async function Dashboard() {
-    const storeToken = (await cookies());
-    const token = storeToken.get("token")?.value;
-    console.log(token);
-    if (!token) {
-        permanentRedirect(new URL("/login").toString());
-    }
 
     return (
         <DashboardClientPage />
